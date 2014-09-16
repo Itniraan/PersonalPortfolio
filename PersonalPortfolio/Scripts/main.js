f36388cd339220cad09b6c5ff65ab42bf1bde939
Itniraan <blakemurdo@gmail.com>
2014-09-16 14:21:45 -0400

    Added decription comments to all files. Also added footer and copyright notice to index.html

--- PersonalPortfolio/Scripts/main.js
+++ PersonalPortfolio/Scripts/main.js
@@ -1,3 +1,10 @@
-﻿$(document).ready( 
+﻿/**
+File Name: main.js
+Author: Blake Murdock
+Website Name: Personal Portfolio for Blake Murdock
+Purpose: This file contains all of the javascript and jQuery functions that are used on my portfolio website
+*/
+
+$(document).ready(
     console.log("jQuery Test!!")
 );
