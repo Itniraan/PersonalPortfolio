513ae34ced928226a964e1521dd44d2c4ac74abf
Itniraan <blakemurdo@gmail.com>
2014-09-26 08:14:02 -0400

    Added breadcrumb navigation for smaller screens, and removed full nav bar for smaller screens

--- PersonalPortfolio/Scripts/main.js
+++ PersonalPortfolio/Scripts/main.js
@@ -5,8 +5,13 @@ Website Name: Personal Portfolio for Blake Murdock
 Purpose: This file contains all of the javascript and jQuery functions that are used on my portfolio website
 */
 
-$(document).ready(
-);
+$(document).ready(function() {
+    // media query event handler
+    if (matchMedia) {
+        var mq = window.matchMedia("(min-width: 500px)");
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+    }})
 
 $("#privacy").click(function () {
     alert("jQuery Test!!");
@@ -15,3 +20,15 @@ $("#privacy").click(function () {
 $("#termsOfUse").click(function () {
     alert("jQuery Test!!");
 });
+
+// media query change
+function WidthChange(mq) {
+
+    if (mq.matches) {
+        console.log("500px");
+    }
+    else {
+        console.log("Less than 500px");
+    }
+
+};
