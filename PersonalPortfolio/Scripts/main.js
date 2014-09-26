cb51511029b6ef2a854ac3c4b3811ba1f0e48a72
Itniraan <blakemurdo@gmail.com>
2014-09-26 16:08:26 -0400

    Added smooth page load effect to JS.

--- PersonalPortfolio/Scripts/main.js
+++ PersonalPortfolio/Scripts/main.js
@@ -5,8 +5,21 @@ Website Name: Personal Portfolio for Blake Murdock
 Purpose: This file contains all of the javascript and jQuery functions that are used by my portfolio website
 */
 
+var speed = 'slow';
+
+$('html, body').hide();
 $(document).ready(function () {
-})
+    $('html, body').fadeIn(speed, function () {
+        $('a[href], button[href]').click(function (event) {
+            var url = $(this).attr('href');
+            if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+            event.preventDefault();
+            $('html, body').fadeOut(speed, function () {
+                window.location = url;
+            });
+        });
+    });
+});
 
 $("#privacy").click(function () {
     alert("jQuery Test!!");
